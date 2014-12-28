cfbb7cf9289ad839be69b42c1f80633bf043d958
Syed Arefinul Haque <arefindk@gmail.com>
2014-12-28 23:06:27 +0600

    Complete step 2

--- simple-todos.js
+++ simple-todos.js
@@ -1,9 +1,20 @@
+// Tasks = new Mongo.collection("tasks");
+
+// if (Meteor.isClient) {
+//   Template.body.helpers({
+//     tasks: function(){
+//       return Tasks.find({});
+//     }
+//   });
+// }
+
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
+  // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"},
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
